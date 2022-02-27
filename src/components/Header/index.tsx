4523b41f91c418afe41db1e5ddcc40aa4123fdf7
Lubni Morais <lubni.morais@gmail.com>
2022-02-27 19:08:16 -0300

    error: click button home page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,19 @@
+import Head from 'next/head';
+
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <Head>
+      <header className={styles.header}>
+        <Link href="/">
+          <a>
+            <img src="/logo.svg" alt="logo" />
+          </a>
+        </Link>
+      </header>
+    </Head>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,17 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
 
+import { useState, useCallback } from 'react';
+
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+
+import { FiCalendar, FiUser } from 'react-icons/fi';
+
+import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../services/prismic';
+
+import Header from '../components/Header';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -19,18 +30,100 @@ interface PostPagination {
   next_page: string;
   results: Post[];
 }
-
 interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  const [nextPage, setNextPage] = useState(postsPagination.next_page);
+  const [currentPage, setCurrentPage] = useState(1);
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  const handleNextPage = useCallback(async () => {
+    if (currentPage !== 1 && nextPage === null) {
+      return;
+    }
 
-//   // TODO
-// };
+    const postResults = await fetch(`${nextPage}`).then(response =>
+      response.json()
+    );
+
+    setNextPage(postResults.next_page);
+    setCurrentPage(postResults.page);
+  }, [currentPage, nextPage]);
+
+  return (
+    <>
+      <Header />
+
+      <main className={commonStyles.container}>
+        <div className={styles.posts}>
+          {postsPagination.results.map(post => (
+            <Link key={post.uid} href={`/post/${post.uid}`}>
+              <a className={styles.post}>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+                <ul>
+                  <li>
+                    <FiCalendar />
+                    {post.first_publication_date}
+                  </li>
+                  <li>
+                    <FiUser />
+                    {post.data.author}
+                  </li>
+                </ul>
+              </a>
+            </Link>
+          ))}
+        </div>
+
+        <button
+          type="button"
+          className={styles.button_posts}
+          onClick={handleNextPage}
+        >
+          Carregar mais posts
+        </button>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      pageSize: 1,
+    }
+  );
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    } as Post;
+  });
+
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: posts,
+  } as PostPagination;
+
+  return {
+    props: {
+      postsPagination,
+    },
+  };
+};
